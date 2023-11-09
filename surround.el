17d855c380155139efc09151e505f78547dc8862
Edvin Syse <es@syse.no>
2023-11-09 12:52:46 +0100

    Add surround-region to transient menus

--- surround.el
+++ surround.el
@@ -70,12 +70,13 @@ before searching forward"
   "Expand region to surrounding bracket-char"
   (interactive "cExpand region to char: \nP")
   (surround--expand-region-fn (lambda() (search-backward (char-to-string bracket-char))) exclusive)
-  (message "e → repeat expansion to %s       r → replace-wrap       u → unwrap       x → auto-expansion" (char-to-string bracket-char))
+  (message "e → repeat expansion to %s       r → replace-wrap       u → unwrap       s → surround-region      x → auto-expansion" (char-to-string bracket-char))
   (set-transient-map
    (let ((map (make-sparse-keymap)))
 	 (define-key map (kbd "e") #'(lambda () (interactive) (surround-expand-region bracket-char exclusive)))
 	 (define-key map (kbd "r") 'surround-replace-wrap)
 	 (define-key map (kbd "u") 'surround-unwrap)
+	 (define-key map (kbd "s") 'surround-region)
 	 (define-key map (kbd "x") #'(lambda () (interactive) (surround-auto-expand-region exclusive)))
 	 map)))
 
@@ -87,11 +88,12 @@ Keep pressing x to continue auto-expanding the region"
   (interactive "*P")
   (surround--expand-region-fn (lambda() (search-backward-regexp
 										 (concat "[" (string-join surround-auto-expand-alist) "]"))) exclusive)
-  (message "x → repeat auto-expansion       r → replace-wrap       u → unwrap       e → expand to other bracket-char")
+  (message "x → repeat auto-expansion       r → replace-wrap       u → unwrap       s → surround-region       e → expand to other bracket-char")
   (set-transient-map
    (let ((map (make-sparse-keymap)))
 	 (define-key map (kbd "x") #'(lambda () (interactive) (surround-auto-expand-region exclusive)))
 	 (define-key map (kbd "r") 'surround-replace-wrap)
+	 (define-key map (kbd "s") 'surround-region)
 	 (define-key map (kbd "u") 'surround-unwrap)
 	 (define-key map (kbd "e") 'surround-expand-region)
 	 map)))
